f87ffb24674549ae1c45d9223cf104f31bc8bf94
Victor Hugo Montano <victorhm2011@gmail.com>
2020-09-05 18:09:00 -0400

    Implement methods and attributes to Sedan

--- Autos/Categories/Sedan.cs
+++ Autos/Categories/Sedan.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Autos.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Autos.Categories
 {
-    class Sedan
+    class Sedan : Car, IElectric
     {
+        private string name { get; set; }
+
+        public Sedan(string  n)
+        {
+            name = n;
+        }
+
+        public int SizeofBattery()
+        {
+            return 450;
+        }
+
+        public int TimeToCharge()
+        {
+            return 8;
+        }
     }
 }
