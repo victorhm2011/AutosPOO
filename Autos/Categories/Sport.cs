6879fc68ddafe727ba89e3727fec822cbb8f2d74
Victor Hugo Montano <victorhm2011@gmail.com>
2020-09-05 18:16:01 -0400

    Implement methods and attributes to Sport

--- Autos/Categories/Sport.cs
+++ Autos/Categories/Sport.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Autos.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Autos.Categories
 {
-    class Sport
+    class Sport : Car, IGas
     {
+        private string name { get; set; }
+
+        private string turbo { get; set; }
+
+        public Sport (string n)
+        {
+            name = n;
+        }
+        void IGas.Maintenance()
+        {
+            turbo = "Ready to run";
+        }
+
+        int IGas.MPG()
+        {
+            return 20;
+        }
     }
 }
