17d84aa3464245a7fb54453a450e82f875279516
Victor Hugo Montano <victorhm2011@gmail.com>
2020-09-05 18:05:52 -0400

    Implement methods and attributes to PickUp

--- Autos/Categories/PickUp.cs
+++ Autos/Categories/PickUp.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Autos.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Autos.Categories
 {
-    class PickUp
+    class PickUp : Car, IGas
     {
+        private string name { get; set; }
+
+        private string cargoSize { get; set; }
+
+
+
+        public PickUp(string n)
+        {
+            name = n;
+
+        }
+
+        public void Maintenance()
+        {
+            cargoSize = "ReadyToWork";
+        }
+
+        public int MPG()
+        {
+            return 18;
+        }
     }
 }
