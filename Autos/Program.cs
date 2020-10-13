89d6c2972d7090f876e65067001665ee1886f8a1
Victor Hugo Montano <victorhm2011@gmail.com>
2020-10-13 12:41:02 -0400

    Add diagram

--- Autos/Program.cs
+++ Autos/Program.cs
@@ -5,13 +5,26 @@ namespace Autos
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            // aqui se crea un auto pero de tipo sedan
             Sedan corolla = new Sedan("corolla");
+            Suv rav4 = new Suv("rav4");
 
             corolla.Transport("Cochabamba", "La Paz");
 
+            corolla.Recharge(100);
+
+            //De acuerdo a que tipo de auto es mostrara el tamaño de la bateria
+
+            Console.WriteLine("Size of corolla");
             Console.WriteLine(corolla.SizeofBattery());
+
+            Console.WriteLine("Size of rav4");
+            Console.WriteLine(rav4.SizeofBattery());
+
+
 
         }
     }
